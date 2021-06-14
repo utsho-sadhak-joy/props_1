d3bf7c2644733f38586b1501a543e7c012659b2b
Joy <utsho233@gmail.com>
2021-06-14 20:25:06 +0600

    adding css & component folder

---
+++ src/Components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CSS/Card.css'
+
+const Card = () => {
+    return (
+        <div>
+            <h1 className="para">Sexy</h1>
+        </div>
+    )
+}
+
+export default Card
