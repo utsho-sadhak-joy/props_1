7a7e4a8adcf8269ebe882da596f4817beae6398f
Joy <utsho233@gmail.com>
2021-06-15 10:21:30 +0600

    successfully writing a state for my react app

---
+++ src/Components/state.js
@@ -0,0 +1,26 @@
+
+
+const food = [
+    {
+        id:'1',
+        title:'Nudles',
+        description:'Last morning i ate nudles',
+    },
+    {
+        id:'2',
+        title:'Rayta',
+        description:'Last day i ate rayta after lunch',
+    },
+    {
+        id:'3',
+        title:'Briyani',
+        description:'Last day i ate Briyani at lunch',
+    },
+    {
+        id:'4',
+        title:'Pork',
+        description:'Last afternoon i ate Pork-fat salad',
+    },
+];
+
+ return {food:food};
