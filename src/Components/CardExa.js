3d87a4853131516dd1346859d7a2a495e4937f92
Joy <utsho233@gmail.com>
2021-06-15 07:51:33 +0600

    only showing image

--- src/Components/CardExa.js
+++ src/Components/CardExa.js
@@ -1,18 +1,13 @@
 import React from 'react'
 import './CSS/CardExa.css'
-// import Dal_Makni from './IMG/Dal_Makni.jpg'
-// import { Button,Card } from 'react-bootstrap'
-import Page from './Page'
 import book from './IMG/pragmatic.jpg'
-import Water from './Water'
+
 
 const CardExa = () => {
     return (
         <div>
             <div className="box"> 
-                <img alt="sadia.img" src={book} className="img" />
-                <Page props="book"/>
-                <Water />
+                <img alt="dal_makhni.img" src={book} className="img" />
             </div>
         </div>
     )
