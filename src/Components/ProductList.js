9afef342304df742525b154b7c33b28c22cd8dbe
Joy <utsho233@gmail.com>
2021-06-15 10:01:53 +0600

    first time understanding parent-child component

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -4,8 +4,21 @@ const ProductList = () => {
     return (
         <div>
             <h1>THis is the first chapter of fulstack react called "votting app"</h1>
+            <Product />
         </div>
     )
 }
 
+
 export default ProductList
+
+
+
+
+const Product = () =>{
+    return(
+        <div>
+            Sadia
+        </div>
+    )
+}
